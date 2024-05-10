14195fdde108ac96b663f6bb1475062978b8711a
German <gerchureloco@gmail.com>
2024-05-10 14:21:04 -0700

    1  2

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+// Return Promise using function getResponseFromAPI()
+export default function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      const cond = true;
+      if (cond) {
+        resolve();
+      } else {
+        reject();
+      }
+    });
+  }
