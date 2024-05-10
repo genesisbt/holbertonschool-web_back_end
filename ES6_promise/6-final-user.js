8c2864cafc6e7f4a45de564f56a4907fd4f1d3b6
German <gerchureloco@gmail.com>
2024-05-10 15:01:18 -0700

    ff

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,6 +2,22 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then(([userResult, photoError]) => [
+      {
+        status: 'done',
+        value: userResult,
+      },
+      {
+        status: 'rejected',
+        value: photoError.message,
+      },
+    ])
+    .catch((error) => [
+      {
+        status: 'rejected',
+        value: error.message,
+      },
+    ]);
 }
