a14775d968eebdec75c4073b857b47b2c1b0a2da
German <gerchureloco@gmail.com>
2024-07-08 18:47:40 +0000

    10 fix lint

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,11 +1,22 @@
 export default class Car {
-    constructor(brand, motor, color) {
-      this._brand = brand;
-      this._motor = motor;
-      this._color = color;
+  constructor(brand, motor, color) {
+    if (typeof brand !== 'string') {
+      throw new TypeError('brand must be a string');
     }
-  
-    cloneCar() {
-      return new Car(this._brand, this._motor, this._color);
+    if (typeof motor !== 'string') {
+      throw new TypeError('motor must be a string');
     }
+    if (typeof color !== 'string') {
+      throw new TypeError('color must be a string');
+    }
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
+
+  cloneCar() {
+    const NewObj = this.constructor[Symbol.species] || this.constructor;
+    const clone = new NewObj();
+    return clone;
+  }
+}
