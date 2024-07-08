efad5cc6d0767db7a54578f28d42586850f3a4ef
German <gerchureloco@gmail.com>
2024-07-08 17:19:30 +0000

    6 fix

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,23 +1,8 @@
 // 6. Handle multiple promises
+
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then(([userResult, photoError]) => [
-      {
-        status: 'done',
-        value: userResult,
-      },
-      {
-        status: 'rejected',
-        value: photoError.message,
-      },
-    ])
-    .catch((error) => [
-      {
-        status: 'rejected',
-        value: error.message,
-      },
-    ]);
+export default function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
+  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
 }
