3e04fba1cb188e9ff97108730ed9412acf63332c
German <gerchureloco@gmail.com>
2024-07-08 19:27:26 +0000

    lint again

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,11 @@
 export default function updateStudentGradeByCity(studentList, city, newGrades) {
-    return studentList
-      .filter(student => student.location === city)
-      .map(student => {
-        const foundGrade = newGrades.find(grade => grade.studentId === student.id);
-        if (foundGrade) {
-          return { ...student, grade: foundGrade.grade };
-        } else {
-          return { ...student, grade: 'N/A' };
-        }
-      });
-  }
+  return studentList
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const foundGrade = newGrades.find((grade) => grade.studentId === student.id);
+      if (foundGrade) {
+        return { ...student, grade: foundGrade.grade };
+      }
+      return { ...student, grade: 'N/A' };
+    });
+}
